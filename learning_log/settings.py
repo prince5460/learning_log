0c046b0e9f8188e2ed2ca5c144ef99fae48c97ad
ZHOU <prince5460@126.com>
2018-01-19 14:24:59 +0800

    增加项目Bootstrap3样式

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'leaning_log',
+        'NAME': 'learning_log',
         'USER': 'root',
         'PASSWORD': 'zhou123',
         'HOST': '',
